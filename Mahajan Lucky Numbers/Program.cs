94ecf9c61b00bcb82854924b2fb060cebf5005d0
mahajanl <lakshamima@gmail.com>
2017-10-06 10:18:02 -0400

    primary commit

---
+++ Mahajan Lucky Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahajan_Lucky_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Why hello there! YOU have been selected for a chance to win a RIDICULOUS AMOUNT OF MONEY! /nI'll let you know how much... at some point... maybe.");
+
+
+        }
+    }
+}
