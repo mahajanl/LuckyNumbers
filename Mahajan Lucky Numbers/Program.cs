f161ece72b0724243ca7e7eff98d57a940173eca
mahajanl <lakshamima@gmail.com>
2017-10-06 14:21:27 -0400

    first full debug of program

--- Mahajan Lucky Numbers/Program.cs
+++ Mahajan Lucky Numbers/Program.cs
@@ -11,75 +11,94 @@ namespace Mahajan_Lucky_Numbers
         static void Main(string[] args)
         {
             Console.Write("Why hello there! YOU have been selected for a chance to win a RIDICULOUS AMOUNT OF MONEY! \nI'll let you know how much... at some point... maybe.\n");
-            Console.WriteLine("First, in order to play, you'll have to give me a TWO numbers, one at a time. This will be the HIGHEST and LOWEST numbers possible. Enter the LOWEST one for me now.");
-            int rangeStart = int.Parse(Console.ReadLine());
-            Console.WriteLine("And now let's get your second, HIGHER (numerically...), number.");
-            int rangeEnd = int.Parse(Console.ReadLine());
+            //PART OF PART IV
+            Console.WriteLine("Would you like a chance to win this jackpot? Even if you get only a FEW numbers right, you'll still win a portion. Type YES if you'd like to play.");
+            string answer = Console.ReadLine().ToLower().Trim();
 
-            Console.WriteLine("Now, let's have a little MORE fun and check on your prescient abilities. Type in the SIX (6) numbers you think will be generated within your number range, hitting ENTER after each number is typed.");
-            int[] userGuess = new int[6];
-
-            for (int i = 0; i <= 5; i++)
-            { Console.WriteLine("Enter a number for me");
-                userGuess[i] = int.Parse(Console.ReadLine());
-
-                if (userGuess[i] >= rangeStart || userGuess[i] <= rangeEnd)
-                { Console.WriteLine("Excellent");
-                }
-
-                else if (userGuess[i] <= rangeStart || userGuess[i] >= rangeEnd)
-                { Console.WriteLine("That's not a number I can use.");
-                  break;
-                }
-            }
-
-            Console.WriteLine("Thanks!");
-            Console.WriteLine("And now the great and powerful Luckster Number Generator will give you 6 numbers.");
-
-            //PART II
-            int[] randoGen = new int[6];
-            Random randoNum = new Random();
-
-            for (int i = 0; i <= 5; i++)
-            { randoGen[i] = randoNum.Next(rangeStart, rangeEnd);
-                Console.WriteLine("Lucky Number: " + randoGen[i]);
-            }
-
-            //PART III
-            int jackpot = 2468975;
-            int numbersRight = 0;
-            
-
-            foreach (int guess in userGuess)
+            while (answer == "yes")
             {
-                if ( guess == randoGen[0])
-                { numbersRight++;
-                }
-                else if (guess == randoGen[1])
-                { numbersRight++;
-                }
-                else if (guess == randoGen[2])
-                { numbersRight++;
-                }
-                else if (guess == randoGen[3])
-                { numbersRight++;
-                }
-                else if (guess == randoGen[4])
-                { numbersRight++;
-                }
-                else if (guess == randoGen[5])
-                { numbersRight++;
-                }
-                else
-                { numbersRight = 0;
+                Console.WriteLine("First, in order to play, you'll have to give me TWO numbers, one at a time. This will be the HIGHEST and LOWEST numbers possible. Enter the LOWEST one for me now.");
+                int rangeStart = int.Parse(Console.ReadLine());
+                Console.WriteLine("And now let's get your second, HIGHER (numerically...), number.");
+                int rangeEnd = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Now, let's have a little MORE fun and check on your prescient abilities. Type in the SIX (6) numbers you think will be generated within your number range, hitting ENTER after each number is typed.");
+                int[] userGuess = new int[6];
+
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine("Enter a number for me");
+                    userGuess[i] = int.Parse(Console.ReadLine());
+
+                    if (userGuess[i] >= rangeStart && userGuess[i] <= rangeEnd)
+                    {
+                        Console.WriteLine("Excellent");
+                    }
+
+                    else if (userGuess[i] <= rangeStart || userGuess[i] >= rangeEnd)
+                    {
+                        Console.WriteLine("That's not a number I can use.");
+
+                        while (userGuess[i] <= rangeStart || userGuess[i] >= rangeEnd)
+                        {
+                            Console.WriteLine("Enter a number for me");
+                            userGuess[i] = int.Parse(Console.ReadLine());
+                        }
+                    }
                 }
 
-                
-            }
-                int userWinnings = jackpot * (1/numbersRight);
+
+                Console.WriteLine("\n\n\n\n\nAnd now the great and powerful Luckster Number Generator will give you 6 numbers.");
+
+                //PART II
+                int[] randoGen = new int[6];
+                Random randoNum = new Random();
+
+                for (int i = 0; i <= 5; i++)
+                {
+                    randoGen[i] = randoNum.Next(rangeStart, rangeEnd);
+                    Console.WriteLine("Lucky Number: " + randoGen[i]);
+                }
+
+                //PART III
+                int jackpot = 2468975;
+                int numbersRight = 0;
+
+                foreach (int guess in userGuess)
+                {
+                    if (guess == randoGen[0])
+                    {
+                        numbersRight++;
+                    }
+                    else if (guess == randoGen[1])
+                    {
+                        numbersRight++;
+                    }
+                    else if (guess == randoGen[2])
+                    {
+                        numbersRight++;
+                    }
+                    else if (guess == randoGen[3])
+                    {
+                        numbersRight++;
+                    }
+                    else if (guess == randoGen[4])
+                    {
+                        numbersRight++;
+                    }
+                    else if (guess == randoGen[5])
+                    {
+                        numbersRight++;
+                    }
+
+
+                }
+                int userWinnings = jackpot / numbersRight;
                 Console.WriteLine("Your winnings are $" + userWinnings + "!");
+                break;
+            }
             
-               
+            Console.WriteLine("Thanks for playing!");
 
 
 
