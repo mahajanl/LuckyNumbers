9dfda3335e42c75a3eda2466d4bea6e66b1ad33b
mahajanl <lakshamima@gmail.com>
2017-10-06 10:28:18 -0400

    part of part one before the array

--- Mahajan Lucky Numbers/Program.cs
+++ Mahajan Lucky Numbers/Program.cs
@@ -10,7 +10,15 @@ namespace Mahajan_Lucky_Numbers
     {
         static void Main(string[] args)
         {
-            Console.Write("Why hello there! YOU have been selected for a chance to win a RIDICULOUS AMOUNT OF MONEY! /nI'll let you know how much... at some point... maybe.");
+            Console.Write("Why hello there! YOU have been selected for a chance to win a RIDICULOUS AMOUNT OF MONEY! \nI'll let you know how much... at some point... maybe.\n");
+            Console.WriteLine("First, in order to play, you'll have to give me a TWO numbers, one at a time. This will be the HIGHEST and LOWEST numbers possible. Enter one for me now.");
+            int rangeStart = int.Parse(Console.ReadLine());
+            Console.WriteLine("And now let's get your second number.");
+            int rangeEnd = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Now, let's have a little MORE fun and check on your prescient abilities. Type in the SIX (6) numbers you think will be generated within your number range.");
+            int [] userGuess = 
+        
 
 
         }
