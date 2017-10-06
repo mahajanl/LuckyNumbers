b336ca5b0d4c8eca39dbf25cce9956f7e84c68d9
mahajanl <lakshamima@gmail.com>
2017-10-06 11:05:33 -0400

    part I finished

--- Mahajan Lucky Numbers/Program.cs
+++ Mahajan Lucky Numbers/Program.cs
@@ -11,14 +11,45 @@ namespace Mahajan_Lucky_Numbers
         static void Main(string[] args)
         {
             Console.Write("Why hello there! YOU have been selected for a chance to win a RIDICULOUS AMOUNT OF MONEY! \nI'll let you know how much... at some point... maybe.\n");
-            Console.WriteLine("First, in order to play, you'll have to give me a TWO numbers, one at a time. This will be the HIGHEST and LOWEST numbers possible. Enter one for me now.");
+            Console.WriteLine("First, in order to play, you'll have to give me a TWO numbers, one at a time. This will be the HIGHEST and LOWEST numbers possible. Enter the LOWEST one for me now.");
             int rangeStart = int.Parse(Console.ReadLine());
-            Console.WriteLine("And now let's get your second number.");
+            Console.WriteLine("And now let's get your second, HIGHER (numerically...), number.");
             int rangeEnd = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Now, let's have a little MORE fun and check on your prescient abilities. Type in the SIX (6) numbers you think will be generated within your number range.");
-            int [] userGuess = 
-        
+            Console.WriteLine("Now, let's have a little MORE fun and check on your prescient abilities. Type in the SIX (6) numbers you think will be generated within your number range, hitting ENTER after each number is typed.");
+            int[] userGuess = new int[6];
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Enter a number for me");
+                userGuess[i] = int.Parse(Console.ReadLine());
+                if (userGuess[i] < 0 || userGuess[i] >= rangeStart || userGuess[i] <= rangeEnd)
+                {
+                    Console.WriteLine("That's not a number I can use here, buddy. Let's try again.");
+                    break;
+                }
+                
+            }
+
+            Console.WriteLine("Thanks!");
+
+
+
+
+
+
+
+
+
+
+
+            //userGuess[0] = int.Parse(Console.ReadLine());
+            //userGuess[1] = int.Parse(Console.ReadLine());
+            //userGuess[2] = int.Parse(Console.ReadLine());
+            //userGuess[3] = int.Parse(Console.ReadLine());
+            //userGuess[4] = int.Parse(Console.ReadLine());
+            //userGuess[5] = int.Parse(Console.ReadLine());
+
+
 
 
         }
